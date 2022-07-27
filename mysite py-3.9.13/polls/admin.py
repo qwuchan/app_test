3c5a317c35dbd1e8137416e1162004ff2a9bdc3b
qwuchan <109152348+qwuchan@users.noreply.github.com>
2022-07-27 18:38:55 +0900

    Add files via upload

---
+++ mysite py-3.9.13/polls/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+ 
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+ 
+ 
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    
+    actions = ['change_title_action'] 
+    
+
+    def change_title_action(self, request, queryset): 
+        queryset.update(title='other')
+    change_title_action.short_description = 'タイトルをotherに変更' 
+
+
+admin.site.register(Question, QuestionAdmin)
